0e4d2a50e66e872a38cd0896a227fe5742f53145
Ben Straub <bs@github.com>
2013-02-15 15:12:50 -0800

    Fix override syntax

--- manifests/l.pp
+++ manifests/l.pp
@@ -1,8 +1,7 @@
 # Laullón's more-current fork. Usage:
 #	include gitx::l
 class gitx::l inherits gitx {
-	package { 'GitX':
-		source   => 'https://github.com/downloads/laullon/gitx/GitX-L_v0.8.4.zip',
-		provider => 'compressed_app';
-	}
+  Package['GitX'] {
+    source => 'https://github.com/downloads/laullon/gitx/GitX-L_v0.8.4.zip',
+  }
 }
