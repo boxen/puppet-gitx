b0df4081fa3913f9f75e12262b410cbcc5c24d13
Ben Straub <bs@github.com>
2013-02-15 15:15:43 -0800

    Add test to verify gitx::l's source.

--- spec/classes/gitx_spec.rb
+++ spec/classes/gitx_spec.rb
@@ -8,3 +8,11 @@ describe 'gitx' do
     })
   end
 end
+
+describe 'gitx::l' do
+  it do
+    should contain_package('GitX').with({
+      :source   => 'https://github.com/downloads/laullon/gitx/GitX-L_v0.8.4.zip',
+    })
+  end
+end
